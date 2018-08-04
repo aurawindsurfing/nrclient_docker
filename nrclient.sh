5e06c205b692ff7e4c7a5709f66b35f294fdd186
Adam Schultz <adam.schultz@live.com>
2018-08-03 22:00:13 -0400

    Update nrclient.sh to add interface route on its own

--- nrclient.sh
+++ nrclient.sh
@@ -53,14 +53,65 @@ fi
 /usr/bin/nrservice -dbroot /config >/dev/null &
 PID=$!
 
+NETWORK=
+IF_IP=
+IF_MASK=
+
 # Register interrupt handler
 trap ctrl_c INT
-function ctrl_c() {
-	kill -9 $PID
+function ctrl_c()
+{
+        kill -9 $PID
+        if [ ! -z "$NETWORK" ] ; then
+                /sbin/ip route del $NETWORK/$IF_MASK dev nrtap
+        fi
 }
 
 # Set nrservice to connect to the configured domain
 /usr/bin/nrclientcmd -d "$NR_DOMAIN" -u "$NR_USER" -p "$NR_USER_PASSWD" -register -dbroot /config
 
-#Wait forever (or until nrservice dies)
+# Functions to manipulate IP strings
+function ip2int()
+{
+	local a b c d
+	{ IFS=. read a b c d; } <<< $1
+	echo $(((((((a << 8) | b) << 8) | c) << 8) | d))
+}
+
+function int2ip()
+{
+	local ui32=$1; shift
+	local ip n
+	for n in 1 2 3 4; do
+		ip=$((ui32 & 0xff))${ip:+.}$ip
+		ui32=$((ui32 >> 8))
+	done
+	echo $ip
+}
+
+function network()
+{
+	local addr=$(ip2int $1); shift
+	local mask=$((0xffffffff << (32 -$1))); shift
+	int2ip $((addr & mask))
+}
+
+# Get the nrtap interface address
+IFADDR=`/sbin/ip addr show dev nrtap | grep 'inet ' | cut -d ' ' -f6`
+while [ -z $IFADDR ]
+do
+	echo "No interface address detected... Retrying in 1 second."
+	sleep 1
+	IFADDR=`/sbin/ip addr show dev nrtap | grep 'inet ' | cut -d ' ' -f6`
+done
+
+# Convert the addr to IP and Mask
+IFS='/' read IF_IP IF_MASK <<< "$IFADDR"; shift
+# Get the network address
+NETWORK=$(network $IF_IP $IF_MASK)
+
+# Add route for interface
+/sbin/ip route add $NETWORK/$IF_MASK dev nrtap
+
+# Wait forever (or until nrservice dies)
 wait $PID
